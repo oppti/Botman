854e60ec194b2d8bc5fadee8ce1f9b220858c67e
Geinian <ti@oppti.com.br>
2020-08-20 13:34:45 -0300

    err TimeOut Mysql

--- Database/database.js
+++ Database/database.js
@@ -1,21 +1,35 @@
 var mysql = require("mysql");
-const connection = mysql.createConnection({
-    host: '192.168.0.72',
-    user: 'root',
-    password: 'root',
-    database: 'Prevendas'
-        // host: 'localhost',
-        // user: 'root',
-        // password: '1234',
-        // database: 'Prevendas'
-
-});
-connection.connect(function(error) {
-    if (error) {
-        console.log("falha ao conectar no mysql:" + error);
-    } else {
-        console.log("MYSQL conectado com sucesso ");
-    }
+const pool = mysql.createPool({
+    host: "192.168.0.72",
+    user: "root",
+    password: "root",
+    database: "Prevendas",
 });
 
-module.exports = connection
+module.exports = {
+    query: function() {
+        var sql_args = [];
+        var args = [];
+        for (var i = 0; i < arguments.length; i++) {
+            args.push(arguments[i]);
+        }
+        var callback = args[args.length - 1]; //last arg is callback
+        pool.getConnection(function(err, connection) {
+            if (err) {
+                console.log(err);
+                return callback(err);
+            }
+            if (args.length > 2) {
+                sql_args = args[1];
+            }
+            connection.query(args[0], sql_args, function(err, results) {
+                connection.release(); // always put connection back in pool after last query
+                if (err) {
+                    console.log(err);
+                    return callback(err);
+                }
+                callback(null, results);
+            });
+        });
+    },
+};
